771cb14029efa8e8598dea8869b999d83c1cb2af
Alex Moisescu <moisescu.alexandru00@gmail.com>
2015-07-07 15:01:49 +0300

    Third commit

--- src/com/company/NaturalSorter.java
+++ src/com/company/NaturalSorter.java
@@ -3,12 +3,11 @@ package com.company;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class NaturalSorter {
-    private static List<String[]> splitStringList;
-    private static List<String> sortedStringList;
-
     public static List<String> sort(List<String> stringList) {
         NaturalComparator comparator = new NaturalComparator();
 
@@ -17,9 +16,21 @@ public class NaturalSorter {
         return stringList;
     }
 
-    private static class NaturalComparator implements  Comparator<String>{
+    private static class NaturalComparator implements Comparator<String> {
         @Override
         public int compare(String o1, String o2) {
+            Boolean compared = false;
+
+            while (!compared) {
+                Pattern patter = Pattern.compile("[0-9]+|[a-zA-Z]+|[^\\s\\w]+");
+                Matcher matcher = patter.matcher(o1);
+                Matcher matcher2 = patter.matcher(o2);
+                matcher.find();
+                System.out.println(matcher.group(0));
+                matcher2.find();
+                System.out.println(matcher.group(0));
+            }
+
             return 0;
         }
     }
