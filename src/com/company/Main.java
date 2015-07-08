21d587f888c112555ad2ab4c484d9d9a7495a6b2
Alex Moisescu <moisescu.alexandru00@gmail.com>
2015-07-08 11:35:22 +0300

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,41 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String[] args) {
-        compare("ceva", "200l");
+
     }
-
-    public static int compare(String o1, String o2) {
-        Boolean compared = false;
-        Pattern pattern = Pattern.compile("[0-9]+|[a-zA-Z]+|[^\\s\\w]+");
-        Matcher matcher = pattern.matcher(o1);
-        Matcher matcher2 = pattern.matcher(o2);
-
-
-        while (matcher2.find() && matcher.find() && !compared) {
-            boolean match =
-        }
-
-        if(matcher.find()) {
-
-        } else if(matcher2.find()) {
-
-        }
-
-        return 0;
-    }
-
-    public static boolean isNumber (String string) {
-        return false;
-    }
-
-    public static boolean isWord(String string) {
-        return false;
-    }
-
-    public static boolean isSymbol(String string) {
-        return false;
-    }
-
 }
--- src/com/company/NaturalSorter.java
+++ src/com/company/NaturalSorter.java
@@ -8,6 +8,10 @@ import java.util.regex.Pattern;
 
 
 public class NaturalSorter {
+    private static Pattern numberPattern = Pattern.compile("[0-9]+");
+    private static Pattern symbolPattern = Pattern.compile("[^\\w]+");
+    private static Pattern pattern = Pattern.compile("[0-9]+|[a-zA-Z]+|[^\\w]+");
+
     public static List<String> sort(List<String> stringList) {
         NaturalComparator comparator = new NaturalComparator();
 
@@ -19,19 +23,68 @@ public class NaturalSorter {
     private static class NaturalComparator implements Comparator<String> {
         @Override
         public int compare(String o1, String o2) {
-            Boolean compared = false;
+            Matcher firstMatcher = pattern.matcher(o1);
+            Matcher secondMatcher = pattern.matcher(o2);
 
-            while (!compared) {
-                Pattern patter = Pattern.compile("[0-9]+|[a-zA-Z]+|[^\\s\\w]+");
-                Matcher matcher = patter.matcher(o1);
-                Matcher matcher2 = patter.matcher(o2);
-                matcher.find();
-                System.out.println(matcher.group(0));
-                matcher2.find();
-                System.out.println(matcher.group(0));
+            while (secondMatcher.find() && firstMatcher.find()) {
+                String firstType = findType(firstMatcher.group(0));
+                String secondType = findType(secondMatcher.group(0));
+                String firstString = firstMatcher.group(0);
+                String secondString = secondMatcher.group(0);
+
+                int result = firstType.compareTo(secondType);
+                if (result != 0) {
+                    return result;
+                }
+
+                switch (firstType) {
+                    case "asymbol":
+                    case "word":
+                        int compareResult = firstString.toLowerCase().compareTo(secondString.toLowerCase());
+                        if (compareResult == 0) {
+                            break;
+                        } else {
+                            return compareResult;
+                        }
+                    case "number":
+                        Integer firstNumber = Integer.valueOf(firstString);
+                        Integer secondNumber = Integer.valueOf(secondString);
+                        int numberResult = firstNumber.compareTo(secondNumber);
+                        if (numberResult == 0) {
+                            break;
+                        } else {
+                            return numberResult;
+                        }
+                }
             }
 
-            return 0;
+            if (firstMatcher.find()) {
+                return 1;
+            } else if (secondMatcher.find()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+
+        public static String findType(String string) {
+            if (isNumber(string)) {
+                return "number";
+            } else if (isSymbol(string)) {
+                return "asymbol";
+            } else {
+                return "word";
+            }
+        }
+
+        public static boolean isNumber(String string) {
+            Matcher numberMatcher = numberPattern.matcher(string);
+            return numberMatcher.find();
+        }
+
+        public static boolean isSymbol(String string) {
+            Matcher symbolMatcher = symbolPattern.matcher(string);
+            return symbolMatcher.find();
         }
     }
 }
